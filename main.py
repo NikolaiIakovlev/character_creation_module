605cecab8c3624f43a5b12ac08f0ee384c503623
Николай Я <no87@yandex.ru>
2022-11-17 21:28:24 +0300

    add pep8

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков атаки в зависимости от
+    выбранного типа персонажа и возвращает строковое сообщение
+    о проведенной атаке.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +20,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков защиты в зависимости
+    от выбранного типа персонажа и возвращает строковое сообщение
+    о выполненном блокировании атак.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал '
                 f'{10 + randint(5, 10)} урона')
@@ -26,6 +36,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """В зависимости от выбранного типа персонажа возвращает
+    сообщение о применении специального умения.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +49,10 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Запускает цыкл тренировки навыков персонажа. В качестве 
+    параметров она получает введенное игроком имя персонажа
+    и выбранный тип персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +77,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Позволяет игроку выбрать тип игрового персонажа
+    и возвращает выбранный вариант.
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -81,7 +101,11 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    """Главная функция. Она запускает игру, и из неё вызываются 
+    все вспомогательные функции.
+    """
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
